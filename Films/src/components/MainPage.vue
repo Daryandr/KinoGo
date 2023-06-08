c39edf298b9e7b4256bc46056da601781c91730a
Dasha <andreeva.d.a9@gmail.com>
2023-06-08 15:06:01 +0300

    clear basic template

---
+++ Films/src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'MainPage',
+
+})
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
