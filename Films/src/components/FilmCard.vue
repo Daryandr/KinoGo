89ad807aefe486d0158b247a1d5cf03940c9f8e6
Dasha <andreeva.d.a9@gmail.com>
2023-06-22 20:31:10 +0300

    add film card component

---
+++ Films/src/components/FilmCard.vue
@@ -0,0 +1,92 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { Film } from "@/types/main";
+import type { PropType } from "vue";
+
+export default defineComponent({
+  name: "FilmCard",
+  props: {
+    film: {
+      type: Object as PropType<Film>,
+      required: true
+    }
+  },
+  methods: {
+    getRatingColor(rating) {
+      if (rating > 7) return "bg-success";
+      else if (rating > 4) return "text-warning";
+      else return "text-danger";
+    },
+    isFilmLiked(id) {
+      return true;///////////
+    }
+  }
+});
+</script>
+
+<template>
+  <div class="card text-white m-3 bg-dark">
+    <img
+      class="card-img-top"
+      :src="film.poster"
+      alt="Failed to load"
+    >
+    <div class="card-img-overlay d-flex flex-column justify-content-end">
+      <div class="d-flex align-items-start flex-wrap">
+        <h4 class="card-title mr">
+          {{ film.name }}
+        </h4>
+        <i
+          class="bi bi-star-fill mr"
+          v-if="isFilmLiked(film._id)"
+        />
+        <h5
+          class="rating"
+          :class="getRatingColor(film.rating.kp)"
+        >
+          {{ film.rating.kp.toFixed(1) }}
+        </h5>
+      </div>
+      <div class="card-text">
+        {{ film.shortDescription }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card-img-overlay {
+  z-index: 1;
+  background: linear-gradient(0deg, $primary 0%, transparent 100%);
+  opacity: 0;
+  transition: opacity 0.3s;
+  font-size: 2.2vmin;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+
+.bi-star-fill {
+  font-size: 2.6vmin;
+  color: $secondary;
+}
+
+.mr {
+  margin-right: 0.5rem;
+}
+
+.card-title {
+  font-size: 3vmin;
+}
+
+.card-img-top {
+  border-radius: 0.5rem;
+}
+
+.rating {
+  padding: 0.2rem;
+  border-radius: 0.5rem;
+  font-size: 2.6vmin;
+}
+</style>
